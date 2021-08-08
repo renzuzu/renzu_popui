52760a9bc7ff5cb05b46bbbb728862b6642306e1
renzuzu <82306584+renzuzu@users.noreply.github.com>
2021-08-08 23:17:29 +0800

    tweak(client): avoid dist bug

--- client.lua
+++ client.lua
@@ -6,14 +6,19 @@ RegisterNUICallback('zone_event', function(data, cb)
 end)
 
 RegisterNUICallback('close', function(data, cb)
+    Wait(200)
     open = false
     SendNUIMessage({type = "reset", content = true})
     SetNuiFocus(false,false)
     SetNuiFocusKeepInput(false)
 end)
 
+local lastpop = nil
+local pop = nil
 RegisterNetEvent('renzu_popui:drawtextuiwithinput')
 AddEventHandler('renzu_popui:drawtextuiwithinput', function(table)
+    open = false
+    pop = table.title
     local t = {
         ['type'] = 'drawtext',
         ['fa'] = table.fa or '<i class="fad fa-sign"></i>',
@@ -25,54 +30,66 @@ AddEventHandler('renzu_popui:drawtextuiwithinput', function(table)
         ['custom_arg'] = table.custom_arg,
         ['key'] = table.key or 'E',
     }
+    Wait(1000)
+    open = true
     SendNUIMessage({type = "inzone", table = t, invehicle = IsPedInAnyVehicle(PlayerPedId())})
     SetNuiFocus(true,false)
     SetNuiFocusKeepInput(true)
+    Wait(1000)
+    lastpop = table.title
 end)
 
 RegisterNetEvent('renzu_popui:showui')
 AddEventHandler('renzu_popui:showui', function(table)
-    open = true
-    local t = {
-        ['type'] = 'normal',
-        ['event'] = table.event,
-        ['title'] = table.title,
-        ['fa'] = table.fa or '<i class="fad fa-sign"></i>',
-        ['server_event'] = table.server_event ~= nil and table.server_event ~= false,
-        ['unpack_arg'] = table.unpack_arg or false,
-        ['invehicle_title'] = table.invehicle_title or false,
-        ['confirm'] = table.confirm or '[ENTER]',
-        ['reject'] = table.reject or '[CLOSE]',
-        ['custom_arg'] = table.custom_arg,
-        ['use_cursor'] = table.use_cursor or false,
-    }
-    SendNUIMessage({type = "inzone", table = t, invehicle = IsPedInAnyVehicle(PlayerPedId())})
-    SetNuiFocus(true,table.use_cursor)
-    SetNuiFocusKeepInput(true)
-    while open and table.use_cursor do
-        DisableControlAction(1, 1, true)
-        DisableControlAction(1, 2, true)
-        DisableControlAction(1, 69, true)
-        DisableControlAction(1, 70, true)
-        DisableControlAction(1, 91, true)
-        DisableControlAction(1, 92, true)
-        DisableControlAction(1, 24, true)
-        DisableControlAction(1, 25, true)
-        DisableControlAction(1, 14, true)
-        DisableControlAction(1, 15, true)
-        DisableControlAction(1, 16, true)
-        DisableControlAction(1, 17, true)
-        DisablePlayerFiring(PlayerId(), true)
-        Wait(1)
+    if not open then
+        open = false
+        pop = table.title
+        local t = {
+            ['type'] = 'normal',
+            ['event'] = table.event,
+            ['title'] = table.title,
+            ['fa'] = table.fa or '<i class="fad fa-sign"></i>',
+            ['server_event'] = table.server_event ~= nil and table.server_event ~= false,
+            ['unpack_arg'] = table.unpack_arg or false,
+            ['invehicle_title'] = table.invehicle_title or false,
+            ['confirm'] = table.confirm or '[ENTER]',
+            ['reject'] = table.reject or '[CLOSE]',
+            ['custom_arg'] = table.custom_arg,
+            ['use_cursor'] = table.use_cursor or false,
+        }
+        SendNUIMessage({type = "inzone", table = t, invehicle = IsPedInAnyVehicle(PlayerPedId())})
+        SetNuiFocus(true,table.use_cursor)
+        SetNuiFocusKeepInput(true)
+        Wait(1000)
+        open = true
+        lastpop = table.title
+        while open and table.use_cursor do
+            DisableControlAction(1, 1, true)
+            DisableControlAction(1, 2, true)
+            DisableControlAction(1, 69, true)
+            DisableControlAction(1, 70, true)
+            DisableControlAction(1, 91, true)
+            DisableControlAction(1, 92, true)
+            DisableControlAction(1, 24, true)
+            DisableControlAction(1, 25, true)
+            DisableControlAction(1, 14, true)
+            DisableControlAction(1, 15, true)
+            DisableControlAction(1, 16, true)
+            DisableControlAction(1, 17, true)
+            DisablePlayerFiring(PlayerId(), true)
+            Wait(1)
+        end
     end
 end)
 
 RegisterNetEvent('renzu_popui:closeui')
 AddEventHandler('renzu_popui:closeui', function(table)
-    open = false
-    SendNUIMessage({type = "reset", content = true})
-    SetNuiFocus(false,false)
-    SetNuiFocusKeepInput(false)
+    if open then
+        open = false
+        SendNUIMessage({type = "reset", content = true})
+        SetNuiFocus(false,false)
+        SetNuiFocusKeepInput(false)
+    end
 end)
 
 function Event(data,custom_arg)
